204c3ab70a60317671699c8ec9c1dc8c66dd82ef
Ivan <ivan.lakhanin@gmail.com>
2020-10-01 16:33:50 +0300

    Add Login logic and validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const {Router} = require('express')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const {check, validationResult} = require('express-validator')
 const User = require('../models/User')
 const router = Router()
@@ -44,7 +46,49 @@ router.post('/register',
 })
 
 // /api/auth
-router.post('/login', async (req, res) => {
+router.post('/login',
+    [
+        check('email', 'Email is invalid').normalizeEmail().isEmail(),
+        check('password', 'Enter the password').exists()
+            .isLength({ min: 6 })
+    ],
+    async (req, res) => {
+    try{
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Invalid email or password'
+            })
+        }
+
+        const {email, password} = req.body
+
+        const user = await User.findOne({ email})
+
+        if(!user) {
+            return res.status(400).json({message: 'User in not find'})
+        }
+        
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch) {
+            return res.status(400).json({message:'Invalid password'})
+        }
+
+        const token = jwt.sign(
+            { userId: user.id },
+            config.get('jwtSecret'),
+            { expiresIn: '1h' }
+        )
+
+        res.json({ token, userId: user.id })
+        
+
+    } catch (e) {
+        res.status(500),json({ message: 'Something went wrong, try again'})
+    }
 
 })
 
