d0dfef1679588c9f281d29f2c4614931bce306f1
Ivan <ivan.lakhanin@gmail.com>
2020-10-01 16:04:04 +0300

    Add registration logic

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 const router = Router()
 
@@ -8,7 +9,17 @@ router.post('/register', async (req, res) => {
     try{
         const {email, password} = req.body
 
+        const candidate = await User.findOne({ email})
 
+        if(candidate) {
+            return res.status(400).json({message: 'This user has been created'})
+        }
+        const hashedPassword = await bcrypt.hash(password, 69)
+        const user = new User({ email, password: hashedPassword})
+
+        await user.save()
+
+        res.status(201).json({message: 'User created !'})
 
     } catch (e) {
         res.status(500),json({ message: 'Something went wrong, try again'})
