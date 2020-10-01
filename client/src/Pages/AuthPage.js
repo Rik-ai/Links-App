27e34ffc9ef9c484b338fb3d51dd5c7d758e32e3
Ivan <ivan.lakhanin@gmail.com>
2020-10-01 17:52:00 +0300

    Working on AuthPage component

--- client/src/Pages/AuthPage.js
+++ client/src/Pages/AuthPage.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AuthPage() {
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    })
+
+    const changeHandler = e => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className='row'>
+            <div className='col s6 offset-s3'>
+                <h1>Cut your so long link !</h1>
+                <div className="card blue-grey darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Autorization</span>
+                        <div>
+
+                        <div className="input-field">
+                            <input
+                                placeholder="Enter your email"
+                                id="email"
+                                type="text"
+                                name='password'
+                                onChange={changeHandler}
+                            />
+                             <label htmlFor="email">Email</label>
+                        </div>
+
+                        <div className="input-field">
+                            <input
+                                placeholder="Enter your password"
+                                id="password"
+                                type="password"
+                                name='password'
+                                onChange={changeHandler}
+                            />
+                             <label htmlFor="password">Password</label>
+                        </div>
+
+
+                        </div>
+                    </div>
+                    <div className="card-action">
+                       <button className='btn green accent-4' style={{margin: 10}} >Sign In</button>
+                       <button className='btn grey lighten-1 black-text'>Sign Up</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
