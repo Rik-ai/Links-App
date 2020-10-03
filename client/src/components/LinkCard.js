d6072dc01a3e3b1bc5f6c82e427009e518588c98
Ivan <ivan.lakhanin@gmail.com>
2020-10-03 19:11:56 +0300

    Create LinkCard component

--- client/src/components/LinkCard.js
+++ client/src/components/LinkCard.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
 export const LinkCard = ({link}) => {
+    return (
+        <>
+            <h2>Link</h2>
+            <p>Your link: <a href={link.to} target='_blank' rel='noopener noreferrer'>{link.to}</a></p>
+            <p>From: <a href={link.from} target='_blank' rel='noopener noreferrer'>{link.from}</a></p>
+            <p>How many clicks on the link: <strong>{link.clicks}</strong></p>
+            <p>Creation date: <strong>{new Date(link.date).toLocaleDateString()}</strong></p>
+        </>
+    )
 
 }
 
